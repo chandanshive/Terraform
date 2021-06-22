67f72925eb416de4deb239d07c92ae337042385d
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-06-22 12:07:21 +0530

    Update Bucket.tf

--- Bucket.tf
+++ Bucket.tf
@@ -1,10 +1,9 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "s3-website-test.hashicorp.com"
-  acl    = "public-read"
-  policy = "${file("policy.json")}"
-  
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
 }
+resource "aws_s3_bucket" "tf_course" {
+  bucket = "tf_course"
+  acl    = "private"
+}
+
